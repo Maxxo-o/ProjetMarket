f032242597a18196cfc103ac382cc061c53928a6
乃0你带我走吧 <57453310+ArreCapClassique@users.noreply.github.com>
2025-01-07 15:42:58 +0100

    modified:   bin/JDBC.class modified:   bin/Main.class modified:   src/JDBC.java modified:   src/Main.java

--- src/JDBC.java
+++ src/JDBC.java
@@ -3,9 +3,11 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
+
+import oracle.jdbc.driver.parser.util.Array;
 
 public class JDBC {
-    private Connection connection;
     private String username;
     private String password;
 
@@ -35,9 +37,52 @@ public class JDBC {
         try (Connection connection = DriverManager.getConnection(
                 "jdbc:oracle:thin:@localhost:1521:xe", username, password)) {
             Statement statement = connection.createStatement();
-            statement.execute(command);
-            ResultSet resultSet = statement.getResultSet();
-            System.out.println(resultSet);
+            boolean hasResultSet = statement.execute(command);
+
+            if (hasResultSet) {
+                ResultSet resultSet = statement.getResultSet();
+                while (resultSet.next()) {
+                    System.out.println(resultSet.getString(1));
+                }
+                System.out.println();
+            } else {
+                System.out.println("Command executed successfully.");
+            }
+        } catch (SQLException e) {
+            System.err.format("SQL State: %s\n%s", e.getSQLState(), e.getMessage());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public ArrayList<ArrayList<String>> executeQuery(String command, int width) {
+        ArrayList<ArrayList<String>> result = new ArrayList<>();
+        try (Connection connection = DriverManager.getConnection(
+                "jdbc:oracle:thin:@localhost:1521:xe", username, password)) {
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery(command);
+            while (resultSet.next()) {
+                ArrayList<String> line = new ArrayList<>();
+                for (int i = 1; i <= width; i++) {
+                    line.add(resultSet.getString(i));
+                }
+                result.add(line);
+            }
+            return result;
+        } catch (SQLException e) {
+            System.err.format("SQL State: %s\n%s", e.getSQLState(), e.getMessage());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public void executeUpdate(String command) {
+        try (Connection connection = DriverManager.getConnection(
+                "jdbc:oracle:thin:@localhost:1521:xe", username, password)) {
+            Statement statement = connection.createStatement();
+            int line = statement.executeUpdate(command);
+            System.out.println(String.format("%s lines affected", line));
         } catch (SQLException e) {
             System.err.format("SQL State: %s\n%s", e.getSQLState(), e.getMessage());
         } catch (Exception e) {
--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,21 @@
+import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-        JDBC database = new JDBC("C##JAVA", "C##JAVA");
-        database.execute("CREATE TABLE avis (\r\n" + //
-                "    CodeP NUMBER(11) NOT NULL,\r\n" + //
-                "    CodeMois NUMBER(11) NOT NULL,\r\n" + //
-                "    TempMin NUMBER(5,2),\r\n" + //
-                "    TempMax NUMBER(5,2),\r\n" + //
-                "    AvisMens VARCHAR2(40),\r\n" + //
-                "    TempMinMoy NUMBER(5,2),\r\n" + //
-                "    TempMaxMoy NUMBER(5,2),\r\n" + //
-                "    PRIMARY KEY (CodeP, CodeMois)\r\n" + //
-                "  )\r\n" + //
-                "");
+        String username = "C##JAVA";
+        String password = "C##JAVA";
+        JDBC admin = new JDBC("system", "oracle");
+        admin.execute(String.format("ALTER USER %s QUOTA UNLIMITED ON USERS", username));
+
+        JDBC database = new JDBC(username, password);
+        database.execute(
+                "BEGIN EXECUTE IMMEDIATE 'DROP TABLE dummy_table CASCADE CONSTRAINTS'; EXCEPTION WHEN OTHERS THEN NULL; END;");
+        database.execute(
+                "BEGIN EXECUTE IMMEDIATE 'CREATE TABLE dummy_table (id NUMBER, name VARCHAR2(100))'; EXCEPTION WHEN OTHERS THEN NULL; END;");
+        database.executeUpdate("INSERT INTO dummy_table (id, name) VALUES (1, 'A')");
+        ArrayList<ArrayList<String>> result = database.executeQuery("SELECT * FROM dummy_table", 2);
+        result.forEach(e -> {
+            e.forEach(e1 -> System.out.printf("%s\t", e1));
+        });
     }
 }
