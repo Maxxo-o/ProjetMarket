c22b59aca7fcd0de2250d9d67231ddb55dd9d51c
unknown <max.segot@gmail.com>
2025-01-07 15:32:47 +0100

    Creation des classes pour le panier profil et client

--- src/Client.java
+++ src/Client.java
@@ -1,2 +1,83 @@
 public class Client {
+    private int idClient;
+    private String Nom;
+    private String Prenom;
+    private int Age;
+    private String Adresse;
+    private Panier panier;
+    private Profil profil;
+
+
+    public Client(int idClient, String Nom, String Prenom, int Age, String Adresse, Panier panier, Profil profil) {
+        this.idClient = idClient;
+        this.Nom = Nom;
+        this.Prenom = Prenom;
+        this.Age = Age;
+        this.Adresse = Adresse;
+        this.panier = panier;
+        this.profil = profil;
+    }
+
+
+
+
+
+
+
+
+    public int getIdClient() {
+        return idClient;
+    }
+
+    public void setIdClient(int idClient) {
+        this.idClient = idClient;
+    }
+
+    public String getNom() {
+        return Nom;
+    }
+
+    public void setNom(String nom) {
+        Nom = nom;
+    }
+
+    public String getPrenom() {
+        return Prenom;
+    }
+
+    public void setPrenom(String prenom) {
+        Prenom = prenom;
+    }
+
+    public int getAge() {
+        return Age;
+    }
+
+    public void setAge(int age) {
+        Age = age;
+    }
+
+    public String getAdresse() {
+        return Adresse;
+    }
+
+    public void setAdresse(String adresse) {
+        Adresse = adresse;
+    }
+
+    public Panier getPanier() {
+        return panier;
+    }
+
+    public void setPanier(Panier panier) {
+        this.panier = panier;
+    }
+
+    public Profil getProfil() {
+        return profil;
+    }
+
+    public void setProfil(Profil profil) {
+        this.profil = profil;
+    }
 }
