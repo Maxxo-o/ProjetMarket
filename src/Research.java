85f7420632b199d9ab05e011bd7b2c0576bbd46a
乃0你带我走吧 <57453310+ArreCapClassique@users.noreply.github.com>
2025-01-15 10:27:27 +0100

    US3.3

--- src/Research.java
+++ src/Research.java
@@ -8,10 +8,12 @@ public class Research {
     public static List<List<String>> searchByKeyword(JDBC database, String keyword) {
         List<List<String>> queryResult = database.executeQuery(
                 """
-                        SELECT Produit.ProduitId, NomProd, PrixAuKg, PrixUnitaire, Poids, Nutriscore, Categorie, Marque, SUM(QteCom) AS \"Quantité vendu\"
+                        SELECT Produit.ProduitId, NomProd, PrixAuKg, PrixUnitaire, Poids, Nutriscore, NomCat, Marque, SUM(QteCom) AS "Quantité vendu"
                         FROM Produit
                         LEFT JOIN Composer ON Produit.ProduitId = Composer.ProduitId
-                        GROUP BY Produit.ProduitId, NomProd, PrixAuKg, PrixUnitaire, Poids, Nutriscore, Categorie, Marque
+                        JOIN Associer ON Produit.ProduitId = Associer.ProduitId
+                        JOIN Categorie ON Associer.CategorieId = Categorie.CategorieId
+                        GROUP BY Produit.ProduitId, NomProd, PrixAuKg, PrixUnitaire, Poids, Nutriscore, NomCat, Marque
                         """);
 
         List<List<String>> result = queryResult
@@ -30,10 +32,12 @@ public class Research {
     public static List<List<String>> listCategory(JDBC database, String keyword) {
         List<List<String>> queryResult = database.executeQuery(
                 """
-                        SELECT Produit.ProduitId, NomProd, PrixAuKg, PrixUnitaire, Poids, Nutriscore, Categorie, Marque, SUM(QteCom) AS \"Quantité vendu\"
+                        SELECT Produit.ProduitId, NomProd, PrixAuKg, PrixUnitaire, Poids, Nutriscore, NomCat, Marque, SUM(QteCom) AS "Quantité vendu"
                         FROM Produit
                         LEFT JOIN Composer ON Produit.ProduitId = Composer.ProduitId
-                        GROUP BY Produit.ProduitId, NomProd, PrixAuKg, PrixUnitaire, Poids, Nutriscore, Categorie, Marque
+                        JOIN Associer ON Produit.ProduitId = Associer.ProduitId
+                        JOIN Categorie ON Associer.CategorieId = Categorie.CategorieId
+                        GROUP BY Produit.ProduitId, NomProd, PrixAuKg, PrixUnitaire, Poids, Nutriscore, NomCat, Marque
                         """);
 
         List<List<String>> result = queryResult
@@ -47,7 +51,7 @@ public class Research {
     // US0.4 Je veux trier une liste de produits.
     public static List<List<String>> orderList(List<List<String>> list, String condition, Boolean croissant) {
         List<String> conditions = Arrays.asList("NomProd", "PrixAuKg", "PrixUnitaire",
-                "Poids", "Nutriscore", "Categorie", "Marque");
+                "Poids", "Nutriscore", "NomCat", "Marque");
         int indexCondition;
 
         if (list != null) {
--- src/Statistique.java
+++ src/Statistique.java
@@ -14,22 +14,22 @@ public class Statistique {
                     SELECT
                         Produit.ProduitId,
                         NomProd,
-                        COUNT(CommandeId) AS \"Nombre de commande\",
-                        SUM(QteCom) AS \"Quantité vendu\",
+                        COUNT(CommandeId) AS "Nombre de commande",
+                        SUM(QteCom) AS "Quantité vendu",
                         SUM(
                             CASE
                                 WHEN PrixUnitaire IS NOT NULL
                                 THEN QteCom * PrixUnitaire
-                                ELSE QteCom * PrixAuKg * Poids
+                                ELSE QteCom * PrixAuKg * Poids / 1000
                             END
-                        ) AS \"CA\"
+                        ) AS "CA"
                     FROM Produit
                     JOIN Composer ON Produit.ProduitId = Composer.ProduitId
                     GROUP BY
                         Produit.ProduitId,
                         NomProd
                     ORDER BY
-                        \"%s\" DESC
+                        "%s" DESC
                     """, conditions.get(condition)));
             return queryResult;
         } else {
@@ -48,23 +48,26 @@ public class Statistique {
         if (conditions.containsKey(condition)) {
             List<List<String>> queryResult = database.executeQuery(String.format("""
                     SELECT
-
-                        COUNT(CommandeId) AS \"Nombre de commande\",
-                        SUM(QteCom) AS \"Quantité vendu\",
+                        Categorie.CategorieId,
+                        NomCat,
+                        COUNT(CommandeId) AS "Nombre de commande",
+                        SUM(QteCom) AS "Quantité vendu",
                         SUM(
                             CASE
                                 WHEN PrixUnitaire IS NOT NULL
                                 THEN QteCom * PrixUnitaire
-                                ELSE QteCom * PrixAuKg * Poids
+                                ELSE QteCom * PrixAuKg * Poids / 1000
                             END
-                        ) AS \"CA\"
-                    FROM Produit
+                        ) AS "CA"
+                    FROM Categorie
+                    JOIN Associer ON Categorie.CategorieId = Associer.CategorieId
+                    JOIN Produit ON Associer.ProduitId = Produit.ProduitId
                     JOIN Composer ON Produit.ProduitId = Composer.ProduitId
                     GROUP BY
-                        Produit.ProduitId,
-                        NomProd
+                        Categorie.CategorieId,
+                        NomCat
                     ORDER BY
-                        \"%s\" DESC
+                        "%s" DESC
                     """, conditions.get(condition)));
             return queryResult;
         } else {
@@ -85,15 +88,15 @@ public class Statistique {
                         Client.ClientId,
                         Nom,
                         Prenom,
-                        COUNT(Commande.CommandeId) AS \"Nombre de commande\",
-                        SUM(QteCom) AS \"Quantité acheté\",
+                        COUNT(Commande.CommandeId) AS "Nombre de commande",
+                        SUM(QteCom) AS "Quantité acheté",
                         SUM(
                             CASE
                                 WHEN PrixUnitaire IS NOT NULL
                                 THEN QteCom * PrixUnitaire
-                                ELSE QteCom * PrixAuKg * Poids
+                                ELSE QteCom * PrixAuKg * Poids / 1000
                             END
-                        ) AS \"CA\"
+                        ) AS "CA"
                     FROM Client
                     JOIN Commande ON Client.ClientId = Commande.ClientId
                     JOIN Composer ON Commande.CommandeId = Composer.CommandeId
@@ -103,12 +106,102 @@ public class Statistique {
                         Nom,
                         Prenom
                     ORDER BY
-                        \"%s\" DESC
+                        "%s" DESC
                     """, conditions.get(condition)));
             return queryResult;
         } else {
             System.err.println("Invalid condition\n");
             return null;
         }
+    }
+
+    public static void general(JDBC database) {
+        String prixMoyenneSurClients = database.executeQuery("""
+                WITH ClientCA(ClientId, CA) AS (
+                    SELECT
+                        Client.ClientId,
+                        SUM(
+                            CASE
+                                WHEN PrixUnitaire IS NOT NULL
+                                THEN QteCom * PrixUnitaire
+                                ELSE QteCom * PrixAuKg * Poids / 1000
+                            END
+                        )
+                    FROM Client
+                    JOIN Commande ON Client.ClientId = Commande.ClientId
+                    JOIN Composer ON Commande.CommandeId = Composer.CommandeId
+                    JOIN Produit ON Composer.ProduitId = Produit.ProduitId
+                    GROUP BY
+                        Client.ClientId
+                )
+                SELECT
+                    CAST(AVG(CA) AS DECIMAL(38,2))
+                FROM
+                    ClientCA
+                """).get(0).get(0);
+
+        String qteMoyenneParCommande = database.executeQuery("""
+                WITH CommandeQte(CommandeId, quentite) AS (
+                    SELECT
+                        CommandeId,
+                        SUM(QteCom)
+                    FROM Composer
+                    JOIN Produit ON Composer.ProduitId = Produit.ProduitId
+                    GROUP BY
+                        CommandeId
+                )
+                SELECT
+                    CAST(AVG(quentite) AS DECIMAL(38,2))
+                FROM
+                    CommandeQte
+                """).get(0).get(0);
+
+        List<List<String>> pourcentageCategorie = database.executeQuery("""
+                WITH CA(CATotal) AS (
+                    SELECT
+                        SUM(
+                            CASE
+                                WHEN PrixUnitaire IS NOT NULL
+                                THEN QteCom * PrixUnitaire
+                                ELSE QteCom * PrixAuKg * Poids / 1000
+                            END
+                        )
+                    FROM Produit
+                    JOIN Composer ON Produit.ProduitId = Composer.ProduitId
+                )
+                SELECT
+                    c.NomCat,
+                    CAST(SUM(
+                        CASE
+                            WHEN PrixUnitaire IS NOT NULL
+                            THEN QteCom * PrixUnitaire
+                            ELSE QteCom * PrixAuKg * Poids / 1000
+                        END
+                    ) * 100 / (SELECT CATotal FROM CA) AS DECIMAL(38,2)) AS "Pourcetage(%)"
+                FROM Categorie
+                JOIN Etre ON Categorie.CategorieId = Etre.CategorieId_SousCategorie
+                JOIN Categorie c ON c.CategorieId = Etre.CategorieId_Principale
+                JOIN Associer ON Categorie.CategorieId = Associer.CategorieId
+                JOIN Produit ON Associer.ProduitId = Produit.ProduitId
+                JOIN Composer ON Produit.ProduitId = Composer.ProduitId
+                GROUP BY
+                    Etre.CategorieId_Principale,
+                    c.NomCat
+                ORDER BY
+                    "Pourcetage(%)" DESC
+                """);
+
+        // Integer length = pourcentageCategorie.stream().mapToInt(e ->
+        // e.get(0).length()).max();
+        System.out.println(String.format("""
+                Prix moyen d'une commande sur tous les clients: %s
+                Quantité moyenne des produits achetés par commande: %s
+                Pourcentage de CA des catégories principales :
+                """,
+                prixMoyenneSurClients,
+                qteMoyenneParCommande));
+        pourcentageCategorie.forEach(e -> {
+            System.out.println(String.format("%35s\t%s", e.get(0), e.get(1)));
+        });
     }
 }
