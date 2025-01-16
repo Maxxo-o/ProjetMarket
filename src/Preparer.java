4d7749f5978911e196d5a89eeb1db07b6401edd0
乃0你带我走吧 <57453310+ArreCapClassique@users.noreply.github.com>
2025-01-16 13:28:05 +0100

    US4

--- src/Preparer.java
+++ src/Preparer.java
@@ -1,19 +1,20 @@
 public class Preparer {
     public static void commanedsAPreparer(JDBC database) {
         database.executeQuery("""
-                    SELECT Commande.*, DateLivraison
+                    SELECT Commande.*, DateLivraison, ModeLivraison
                     FROM Commande
-                    LEFT JOIN Delivrer ON Commande.CommandeId = Delivrer.CommandeId
+                    JOIN Delivrer ON Commande.CommandeId = Delivrer.CommandeId
                     WHERE EtatCom = 'En preparation'
                     ORDER BY DateLivraison
                 """).forEach(System.out::println);
     }
 
     public static void marquerCommande(JDBC database, int CommandeId, String EtatCom) {
+        String fin = EtatCom.equals("Finalisee") ? "HeureFinCommande = SYSDATE," : "";
         database.executeUpdate(String.format("""
                 UPDATE Commande
-                SET EtatCom = %s
+                SET %sEtatCom = %s
                 WHERE CommandeId = %s
-                """, EtatCom, CommandeId));
+                """, fin, EtatCom, CommandeId));
     }
 }
